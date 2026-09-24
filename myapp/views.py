422c14aad229fbdabd9b6233127b2e04c7328b24
TREYRERI <kholovamunavvara00@gmail.com>
2026-09-25 03:33:21 +0500

    added crud views and permissions for Lesson model

--- myapp/views.py
+++ myapp/views.py
@@ -63,3 +63,58 @@ def course_delete(request, id):
         course.delete()
         return redirect('course_list')
     return render(request, 'course_delete.html' , {'course' : course})
+
+
+
+#lesson
+@permission_required('myapp.view_lesson' , raise_exception = True)
+def lesson_list(request):
+    lessons = Lesson.objects.all()
+
+    return render(request, 'lesson_list.html' , {'lessons' : lessons})
+
+@permission_required('myapp.view_lesson', raise_exception = True)
+def lesson_detail(request ,id):
+    lesson = get_object_or_404(Lesson, id = id )
+    return render (request, 'lesson_detail.html', {'lesson' : lesson})
+
+@permission_required('myapp.add_lesson', raise_exception = True)
+def lesson_create(request):
+    courses = Course.objects.all()
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        course_id = request.POST.get('course')
+
+        course = get_object_or_404(Course, id = course_id)
+
+        Lesson.objects.create(title = title , content = content, course = course)
+        return redirect('lesson_list')
+    return render(request, 'lesson_form.html' , {'courses' : courses})
+
+@permission_required('myapp.change_lesson', raise_exception=True)
+def lesson_update(request, id):
+    lesson = get_object_or_404(Lesson, id = id)
+    courses = Course.objects.all()
+
+    if request.method == 'POST':
+        lesson.title = request.POST.get('title')
+        lesson.content = request.POST.get('content')
+        course_id = request.POST.get('course')
+
+        lesson.course = get_object_or_404(Course, id=course_id)
+
+        lesson.save()
+
+        return redirect('lesson_detail' , id = lesson.id)
+    return render(request, 'lesson_form.html' , {'lesson' : lesson, 'courses' : courses})
+
+@permission_required('myapp.delete_lesson', raise_exception = True)
+def lesson_delete(request, id):
+    lesson = get_object_or_404(Lesson, id = id)
+    if request.method == 'POST':
+        lesson.delete()
+        return redirect('lesson_list')
+    return render(request, 'lesson_delete.html' , {'lesson' : lesson} )
+
